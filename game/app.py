6f80eee04b5f5633bb37fc32232dd8ec3eadf745
imhonggeun <dkfl9870@gmail.com>
2025-06-30 11:14:19 +0900

    append 사용 및 \n 줄바꿈 추가

--- game/app.py
+++ game/app.py
@@ -1,17 +1,26 @@
 print("안녕하세요")
 
-#파일생성 및 내용추가
+#예제1 파일 안에 내용 보기
+#file = open('README.md' ,encoding="UTF-8")
+#print(type(file.readlines()))
+#for row in file.readlines() :
+#    print(row)
+
+#예제2 파일생성 및 내용추가
 #1.파일 생성 및 읽기 모드는 동시에 사용이 안됨
 file = open('game\\text.txt', encoding='UTF-8', mode='w') #쓰기
-file.write('안녕하세요') #글자 생성
+file.write('안녕하세요\n') #글자 생성
 file.close()# 쓰기 이후에 보기를 위해서 파일을 닫는다
 
 file = open('game\\text.txt', encoding='UTF-8', mode='r') #읽기
 print(file.readlines())
 file.close() #메모리를 사용안하기위해서 필요하다
 
-#파일 안에 내용 보기
-#file = open('README.md' ,encoding="UTF-8")
-#print(type(file.readlines()))
-#for row in file.readlines() :
-#    print(row)
+#예제3 파일내용 추가
+file = open('game\\text.txt', encoding='UTF-8', mode='a')# appen 내용추가
+file.write('내용추가\n')# appen 사용시 write 사용
+file.close()
+
+file = open('game\\text.txt', encoding='UTF-8', mode='r')
+print(file.readlines())
+file.close()
