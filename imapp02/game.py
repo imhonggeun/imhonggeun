08bedf0b68720d3b9ad763e58214b8d133f9a376
imhonggeun <dkfl9870@gmail.com>
2025-07-01 17:06:51 +0900

    기본 개념알기

--- imapp02/game.py
+++ imapp02/game.py
@@ -13,12 +13,32 @@ conn= mariadb.connect(**conn_params)
 
 cur= conn.cursor()
 
-sql = 'SELECT  `no`,  `title`,  `desc`, LEFT(`content`, 256),  `regDate`,  `modDate` FROM `edu`.`NOTICE` LIMIT 1000;'
+# sql = 'SELECT  `no`,  `title`,  `desc`, LEFT(`content`, 256),  `regDate`,  `modDate` FROM `edu`.`NOTICE` LIMIT 1;'
 
+sql ='SELECT * FROM `edu`.`NOTICE` WHERE NO = 1' #변경
 cur.execute(sql)
-result = cur.fetchall()
+result = cur.fetchone() #fetchall = list , fetchone = tuple
+col_name=cur.description
 
-print(result)
+print(col_name[0])
+
+# name = ''
+# for row in col_name:
+#     name += row[0] + ("\t\t\t" if row[0] == 'regDate' else "\t")
+# print(name)
+# if result == None:
+#     print("데이터 없습니다.")
+# else:
+#     row=''
+#     #print('no\ttitle\tdesc\tcontent\tregDate\t\t\tmodDate')
+#     for col in result:
+#         #print(col)
+#         if col == 'None':
+#             row += ('없음\t')
+#         else:
+#             row += (f'{col}\t')
+#     print(row)
+# print(type(result))
 
 cur.close()
 conn.close()
