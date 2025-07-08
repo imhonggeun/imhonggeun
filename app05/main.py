93a4f3dbb32f612159ac515f6b7c98264d06c2bc
imhonggeun <dkfl9870@gmail.com>
2025-07-08 09:35:12 +0900

    펑션 로컬호스트 연결

--- app05/main.py
+++ app05/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
 from controller import home
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(home.controller)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
