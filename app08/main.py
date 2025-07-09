da5f9a69eedd59c3c591990be5b1b0ec07ae1970
imhonggeun <dkfl9870@gmail.com>
2025-07-09 13:10:25 +0900

    sql로 실제 데이터 가져오기&mariadb연동 및 uv에서 설치방법:uv add 설치할파일명

--- app08/main.py
+++ app08/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Form
+from typing import Annotated
+import mariadb,os
 app = FastAPI()
 
 @app.get("/")
@@ -8,8 +9,56 @@ def root():
 
 @app.get("/test")
 def test(a : int):
-    return{"A변수" : a}
+    return {"A변수" : a}
 
+@app.post("/test")
+def tset(name : Annotated[str,Form()]):
+    return {"이름" : name}
+
+@app.get("/db")
+def db():
+    conn_params = {
+        "user" : os.getenv('MARIADB_USER'),
+        "password" : os.getenv('MARIADB_PASSWORD'),
+        "host" : os.getenv('MARIADB_HOST'),
+        "database" : os.getenv('MARIADB_DATABASE'),
+        "port" : int(os.getenv('MARIADB_PORT')),    
+    }
+    
+    conn = mariadb.connect(**conn_params)
+    cur = conn.cursor()
+    
+    sql = "select * from movies"
+    cur.execute(sql)
+    result = cur.fetchall()
+    
+    cur.close()
+    conn.close()
+    return {"result" : result}
+
+@app.get("/year")
+def year( y : str):
+    conn_params = {
+        "user" : os.getenv('MARIADB_USER'),
+        "password" : os.getenv('MARIADB_PASSWORD'),
+        "host" : os.getenv('MARIADB_HOST'),
+        "database" : os.getenv('MARIADB_DATABASE'),
+        "port" : int(os.getenv('MARIADB_PORT')),    
+    }
+    
+    conn = mariadb.connect(**conn_params)
+    cur = conn.cursor()
+    sql = f"SELECT * FROM movies WHERE YEAR < '{y}';"
+
+    cur.execute(sql)
+    result = cur.fetchall()
+    
+    cur.close()
+    conn.close()
+    return {"result" : result}
+    
+
+#}
 #
 # def main():
 #     print("Hello from app08!")
