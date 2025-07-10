f50b3eda0e7a9b5a8695c2c94b799f626d4557f2
imhonggeun <dkfl9870@gmail.com>
2025-07-10 11:05:41 +0900

    movie 상세 페이지 기능 추가

--- PythonStudy-app09/PythonStudy-app09/app09/front_end/js/movie.js
+++ PythonStudy-app09/PythonStudy-app09/app09/front_end/js/movie.js
@@ -1,3 +1,27 @@
 $(function(){
+    var imdbID = new URL(location.href).searchParams.get("imdbID")
+    const view = (res) => {
+        console.log(res)
+        if(res.Response === 'True') {
+            var html = `<h2>${res.Title}<span>${res.Type}</span></h2>
+                        <p>${res.Year}</p>
+                        <p>${res.Country}</p>
+                        <img src="${res.Poster}" alt="Movie Poster" class="featured-img" />
+                        <p>${res.Plot}</p>
+                        `
+            $("#movie-body").html(html)
+        } else {
+            var html = `<h2>선택한 내용이 없습니다.</h2>
+            <p>다시 검색하세요.</p>`
+            $("#movie-error").html(html)
+        }
+    }
     
+    if(imdbID === null) {
+        Document.location.href = "index.html"
+    } else {    
+        $.post('http://localhost:8000/movie/findOne', {'id': imdbID})
+        .done(view)
+        .fail((e) => console.error(e))
+    }
 })
